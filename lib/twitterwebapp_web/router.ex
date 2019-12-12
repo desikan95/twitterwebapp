37b2b38d8ee922b4ca66c9f7bb4c4482001aa4ad
desikan95 <desikan95@gmail.com>
2019-12-12 13:17:18 -0500

    Channels

---
+++ lib/twitterwebapp_web/router.ex
@@ -0,0 +1,27 @@
+defmodule TwitterwebappWeb.Router do
+  use TwitterwebappWeb, :router
+
+  pipeline :browser do
+    plug :accepts, ["html"]
+    plug :fetch_session
+    plug :fetch_flash
+    plug :protect_from_forgery
+    plug :put_secure_browser_headers
+  end
+
+  pipeline :api do
+    plug :accepts, ["json"]
+  end
+
+  scope "/", TwitterwebappWeb do
+    pipe_through :browser
+
+    get "/", PageController, :index
+    resources "/users", UserController
+  end
+
+  # Other scopes may use custom stacks.
+  # scope "/api", TwitterwebappWeb do
+  #   pipe_through :api
+  # end
+end
